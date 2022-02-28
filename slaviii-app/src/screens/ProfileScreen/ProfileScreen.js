d993c132fa07e00c383b62169fa17953fefbb025
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-03-01 04:17:00 +0700

    Add pet list and FAB menu in profile screen

--- slaviii-app/src/screens/ProfileScreen/ProfileScreen.js
+++ slaviii-app/src/screens/ProfileScreen/ProfileScreen.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
-import { Text, View, Image, TouchableOpacity, Alert } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Alert,
+  FlatList,
+} from "react-native";
 import styles from "./styles";
 import { firebase } from "../../firebase/config";
+import { Feather } from "@expo/vector-icons";
+import { FloatingAction } from "react-native-floating-action";
 
 export default function ProfileScreen({ navigation }) {
   const signOutUser = async () => {
@@ -25,23 +34,160 @@ export default function ProfileScreen({ navigation }) {
       { text: "OK", onPress: () => signOutUser() },
     ]);
 
+  const petList = {
+    calls: [
+      {
+        id: 1,
+        name: "Mark Doe",
+        status: "active",
+        image: "https://bootdey.com/img/Content/avatar/avatar7.png",
+      },
+      {
+        id: 2,
+        name: "Clark Man",
+        status: "active",
+        image: "https://bootdey.com/img/Content/avatar/avatar6.png",
+      },
+      {
+        id: 3,
+        name: "Jaden Boor",
+        status: "active",
+        image: "https://bootdey.com/img/Content/avatar/avatar5.png",
+      },
+      {
+        id: 4,
+        name: "Srick Tree",
+        status: "active",
+        image: "https://bootdey.com/img/Content/avatar/avatar4.png",
+      },
+      {
+        id: 5,
+        name: "Erick Doe",
+        status: "active",
+        image: "https://bootdey.com/img/Content/avatar/avatar3.png",
+      },
+      {
+        id: 6,
+        name: "Francis Doe",
+        status: "active",
+        image: "https://bootdey.com/img/Content/avatar/avatar2.png",
+      },
+      {
+        id: 8,
+        name: "Matilde Doe",
+        status: "active",
+        image: "https://bootdey.com/img/Content/avatar/avatar1.png",
+      },
+      {
+        id: 9,
+        name: "John Doe",
+        status: "active",
+        image: "https://bootdey.com/img/Content/avatar/avatar4.png",
+      },
+      {
+        id: 10,
+        name: "Fermod Doe",
+        status: "active",
+        image: "https://bootdey.com/img/Content/avatar/avatar7.png",
+      },
+      {
+        id: 11,
+        name: "Danny Doe",
+        status: "active",
+        image: "https://bootdey.com/img/Content/avatar/avatar1.png",
+      },
+    ],
+  };
+
+  const profileActions = [
+    {
+      text: "Announcement",
+      icon: <Feather name="alert-circle" size={24} color="white" />,
+      name: "bt_annoucement",
+      position: 1,
+      color: "#C84132",
+    },
+    {
+      text: "Edit Profile",
+      icon: <Feather name="edit" size={24} color="white" />,
+      name: "bt_editprofile",
+      position: 2,
+      color: "#C84132",
+    },
+    {
+      text: "Log out",
+      icon: <Feather name="log-out" size={24} color="white" />,
+      name: "bt_logout",
+      position: 3,
+      color: "#C84132",
+    },
+  ];
+
+  const renderItem = ({ item }) => {
+    return (
+      <TouchableOpacity>
+        <View style={styles.row}>
+          <Image source={{ uri: item.image }} style={styles.pic} />
+          <View>
+            <View style={styles.nameContainer}>
+              <Text
+                style={styles.nameTxt}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                {item.name}
+              </Text>
+              <Text style={styles.mblTxt}>Mobile</Text>
+            </View>
+            <View style={styles.msgContainer}>
+              <Text style={styles.msgTxt}>{item.status}</Text>
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}></View>
-      <Image
-        style={styles.avatar}
-        // source={{ uri: "https://bootdey.com/img/Content/avatar/avatar6.png" }}
-        source={require("../../../assets/icon.png")}
-      />
-      <View style={styles.body}>
-        <View style={styles.bodyContent}>
+      <View style={styles.header}>
+        <View style={styles.headerContent}>
+          {/* <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar1.png'}}/> */}
+          <Image
+            style={styles.avatar}
+            source={require("../../../assets/icon.png")}
+          />
           <Text style={styles.name}>John Doe</Text>
-          <Text style={styles.info}>UX Designer / Mobile developer</Text>
-          <Text style={styles.description}>
-            Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum
-            electram expetendis, omittam deseruisse consequuntur ius an,
-          </Text>
-
+        </View>
+      </View>
+      <View style={{ flex: 2 }}>
+        <FlatList
+          extraData={petList}
+          data={petList.calls}
+          keyExtractor={(item) => {
+            return item.id;
+          }}
+          renderItem={renderItem}
+        />
+      </View>
+      <FloatingAction
+        color="#C84132"
+        actions={profileActions}
+        onPressItem={(name) => {
+          console.log(`selected button: ${name}`);
+          if (name === "bt_logout") {
+            confirmSignOutAlert();
+          } else if (name === "bt_editprofile") {
+            alert("Edit Profile");
+            // navigation.navigate("EditProfile");
+          } else if (name === "bt_annoucement") {
+            alert("Announcement");
+            // navigation.navigate("Announcement");
+          }
+        }}
+      />
+      {/* <View style={styles.body}>
+        <View style={styles.bodyContent}>
           <TouchableOpacity
             style={styles.buttonContainer}
             onPress={() => {
@@ -54,7 +200,7 @@ export default function ProfileScreen({ navigation }) {
             <Text>Opcion 2</Text>
           </TouchableOpacity>
         </View>
-      </View>
+      </View> */}
     </View>
   );
 }
--- slaviii-app/src/screens/ProfileScreen/styles.js
+++ slaviii-app/src/screens/ProfileScreen/styles.js
@@ -1,20 +1,24 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   header: {
-    backgroundColor: "#00BFFF",
-    height: 200,
+    flex: 1,
+    backgroundColor: "#20B2AA",
+  },
+  headerContent: {
+    padding: 30,
+    alignItems: "center",
   },
   avatar: {
     width: 130,
     height: 130,
     borderRadius: 63,
     borderWidth: 4,
-    borderColor: "white",
+    borderColor: "#FFFFFF",
     marginBottom: 10,
-    alignSelf: "center",
-    position: "absolute",
-    marginTop: 130,
   },
   name: {
     fontSize: 22,
@@ -25,25 +29,9 @@ export default StyleSheet.create({
     marginTop: 40,
   },
   bodyContent: {
-    flex: 1,
+    // flex: 1,
     alignItems: "center",
     padding: 30,
-  },
-  name: {
-    fontSize: 28,
-    color: "#696969",
-    fontWeight: "600",
-  },
-  info: {
-    fontSize: 16,
-    color: "#00BFFF",
-    marginTop: 10,
-  },
-  description: {
-    fontSize: 16,
-    color: "#696969",
-    marginTop: 10,
-    textAlign: "center",
   },
   buttonContainer: {
     marginTop: 10,
@@ -55,5 +43,45 @@ export default StyleSheet.create({
     width: 250,
     borderRadius: 30,
     backgroundColor: "#00BFFF",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderColor: "#DCDCDC",
+    backgroundColor: "#fff",
+    borderBottomWidth: 1,
+    padding: 10,
+  },
+  pic: {
+    borderRadius: 30,
+    width: 60,
+    height: 60,
+  },
+  nameContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: 280,
+  },
+  nameTxt: {
+    marginLeft: 15,
+    fontWeight: "600",
+    color: "#222",
+    fontSize: 18,
+    width: 170,
+  },
+  mblTxt: {
+    fontWeight: "200",
+    color: "#777",
+    fontSize: 13,
+  },
+  msgContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  msgTxt: {
+    fontWeight: "400",
+    color: "#008B8B",
+    fontSize: 12,
+    marginLeft: 15,
   },
 });
