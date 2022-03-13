b4817403ef8127908196c9c3fe35864a527bd145
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-03-14 04:53:07 +0700

    Add logo

--- slaviii-app/src/screens/ForgotPasswordScreen/styles.js
+++ slaviii-app/src/screens/ForgotPasswordScreen/styles.js
@@ -10,7 +10,8 @@ export default StyleSheet.create({
     height: 160,
     width: 160,
     alignSelf: "center",
-    marginTop: 30,
+    marginTop: 40,
+    marginBottom: 30,
     tintColor: "#6B1C12",
   },
   errorView: {
